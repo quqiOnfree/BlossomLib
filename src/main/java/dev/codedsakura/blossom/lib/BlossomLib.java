61eeaae1c3917f8b756af84fc9440864328d61c2
codedsakura <sakuracoded@gmail.com>
2022-04-09 18:21:20 +0300

    create project

---
+++ src/main/java/dev/codedsakura/blossom/lib/BlossomLib.java
@@ -0,0 +1,10 @@
+package dev.codedsakura.blossom.lib;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BlossomLib implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
